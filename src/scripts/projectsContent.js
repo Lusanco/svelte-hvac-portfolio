6f3c0d9a543495949aae5aa43ac416fc3e7a2e70
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-09-11 14:28:56 -0400

    update - add img paths

--- src/scripts/projectsContent.js
+++ src/scripts/projectsContent.js
@@ -1,11 +1,44 @@
 const projects = {
-  "Mini-VRF CU view.png": "Aircon brand VRF system installation, using cassettes and high wall evaporators. Mechanical shop offices located at an airport.",
-  "VRF CUs back view.jpg": "Trane brand VRF system installation for a high-end residence.",
-  "New replaced compressors and structural bases (1).jpg": "120 Tons Trane air cooled chiller repairs. Custom fabricated steel base structures for compressors, valves, 3 of the 4 scroll compressors, and other mechanical and electrical elements were replaced. Project is located at one of the main university campuses in Puerto Rico.",
-  "Univ. ceiling VRF cassette.jpg": "Trane VRF system installation for classroom facilities.",
-  "Eng. Design.png": "Engineering design for an entertainment project in Puerto Rico.",
-  "RTU final view.jpg": "Trane roof top packaged unit replacement at a major university located in Puerto Rico.  ",
-  "New AHU coil.jpg": "Trane 12.5 Tons condensing unit replacement, and air handling unit coil replacement. New ref pipes and valves, among other elements and tasks. Investigative laboratory application.",
+  "Mini-VRF CU view.png":
+    "Aircon brand VRF system installation, using cassettes and high wall evaporators. Mechanical shop offices located at an airport.",
+  "VRF CUs back view.jpg":
+    "Trane brand VRF system installation for a high-end residence.",
+  "New replaced compressors and structural bases (1).jpg":
+    "120 Tons Trane air cooled chiller repairs. Custom fabricated steel base structures for compressors, valves, 3 of the 4 scroll compressors, and other mechanical and electrical elements were replaced. Project is located at one of the main university campuses in Puerto Rico.",
+  "Univ. ceiling VRF cassette.jpg":
+    "Trane VRF system installation for classroom facilities.",
+  "Eng. Design.png":
+    "Engineering design for an entertainment project in Puerto Rico.",
+  "RTU final view.jpg":
+    "Trane roof top packaged unit replacement at a major university located in Puerto Rico.  ",
+  "New AHU coil.jpg":
+    "Trane 12.5 Tons condensing unit replacement, and air handling unit coil replacement. New ref pipes and valves, among other elements and tasks. Investigative laboratory application.",
+};
+
+const projectsImgPaths = {
+  1: [
+    "Mini-VRF CU view.png",
+    "Plenum view ref. & elect. piping (1).jpg",
+    "Plenum view ref. & elect. piping (2).jpg",
+  ],
+  2: ["VRF CUs back view.jpg"],
+  3: [
+    "New replaced compressors and structural bases (1).jpg",
+    "Chiller structural base and compressor zoomed view.jpg",
+    "Old damaged chiller compressors.jpg",
+  ],
+  4: [
+    "Univ. ceiling VRF cassette.jpg",
+    "Univ. VRF CUs, Pipes, and Elect. view.jpg",
+  ],
+  5: ["Eng. Design.png"],
+  6: ["RTU final view.jpg", "RTU at crane lift.jpg", "RTU in progress.jpg"],
+  7: [
+    "New AHU coil.jpg",
+    "Coil drawing.png",
+    "New CU.jpg",
+    "New ref. pipes, anchor bases, and valves.jpg",
+  ],
 };
 
 export default projects;
