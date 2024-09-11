3a2914e1af0437b595e436383424a1bd09eb4336
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-09-11 14:27:58 -0400

    update - add title to projects

--- src/components/ProjectsGallery.svelte
+++ src/components/ProjectsGallery.svelte
@@ -4,11 +4,14 @@
 </script>
 
 <section
-  class="flex flex-wrap justify-center w-full gap-4 px-4 py-16 m-auto bg-slate-200"
+  class="flex flex-col justify-center w-full gap-8 px-4 py-16 m-auto bg-slate-200"
 >
-  {#each Object.entries(projects) as [pathName, pathDescription], index}
-    {#if index + 1 <= Object.keys(projects).length}
-      <ImgCard pathFolder={index + 1} {pathName} {pathDescription} />
-    {/if}
-  {/each}
+  <h1 class="text-4xl font-bold text-center text-gunmetal/80">Projects</h1>
+  <div class="flex flex-wrap justify-center w-full gap-4 m-auto">
+    {#each Object.entries(projects) as [pathName, pathDescription], index}
+      {#if index + 1 <= Object.keys(projects).length}
+        <ImgCard pathFolder={index + 1} {pathName} {pathDescription} />
+      {/if}
+    {/each}
+  </div>
 </section>
