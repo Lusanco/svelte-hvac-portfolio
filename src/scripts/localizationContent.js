1913f344d79c8cb0b62585be1fb5b52ed9e6ee87
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-09-14 23:17:38 -0400

    wip - localization contents

---
+++ src/scripts/localizationContent.js
@@ -0,0 +1,67 @@
+const EN = {
+  header: {
+    aboutUs: "About Us",
+    services: "Services",
+    projects: "Projects",
+    education: "Education",
+    contactUs: "Contact Us",
+  },
+  indexHero: {
+    h1a: "Beyond HVAC",
+    h3: "Comprehensive Building Solutions",
+    p: "From HVAC to energy optimization, we’ve got you covered. Our team of experienced professionals offers comprehensive solutions tailored to your building’s specific needs. We specialize in HVAC systems, energy audits, and building automation, ensuring optimal efficiency and comfort. With a focus on quality, reliability, and customer satisfaction, we’re committed to delivering exceptional results for your building.",
+    a: "Contact Us",
+    h1b: "Innovative HVAC Solutions & More!",
+  },
+  indexAbout: {
+    h1: "About Us",
+    p: "Discover how 828 LLC provides innovative HVAC and mechanical solutions tailored to your needs. Learn about our commitment to quality, reliability, and customer satisfaction.",
+    a: "Learn More",
+  },
+  indexServices: {
+    h1: "Services",
+    p: "Discover our comprehensive range of HVAC and mechanical services, including installation, maintenance, energy efficiency, and custom solutions.",
+    a: "Our Services",
+  },
+  indexProjects: {
+    h1: "Projects",
+    p: "Explore our successful projects in various industries, including commercial, industrial, residential, and educational. See how we've delivered innovative solutions and exceeded our clients' expectations.",
+    a: "Our Projects",
+  },
+  about828: {
+    h1: "828 LLC",
+    p1: "828 LLC is a company dedicated to mechanical installations, mostly related to the field of HVAC (Heating, Ventilation & Air Conditioning). We offer engineering services related to this field and Energy. We work in the installation and general execution of projects, and we have extensive knowledge in basically all types of air conditioning and ventilation equipment. By this, we mean Chillers, VRF, Packaged Units, Commercial and Residential Splits, Air Distribution, Controls, among others.",
+    p2: "Customer service is key to our operations, and we believe in always maintaining the highest level. We listen to our client and are genuinely interested in knowing their requirements. This is how we create a communicative and efficient work team with them, which results in viable solutions in every sense.",
+    p3: "Ethics and integrity are pillars in our company.",
+  },
+  aboutMission: {
+    h1: "Our Mission",
+    p: "We provide the world viable, reliable, and profitable technological solutions. We create total trust in our clients by inspiring them with our knowledge in the field of engineering, with our integrity, and with our intention to form a winning team with them.",
+  },
+  aboutVision: {
+    h1: "Our Vision",
+    p: "To maintain a culture and organizational structure based on thinking differently, on imagination, and on maintaining the highest standard of customer service. Provide the tools and develop empowerment in our employees, who will organically transmit the same culture to our clients.",
+  },
+  aboutValues: {
+    h1: "Our Values",
+    dt1: "Integrity",
+    dd1: "leading to total trust.",
+    dt2: "Empathy",
+    dd2: "that understands and respects.",
+    dt3: "Innovation",
+    dd3: "that advances and transforms.",
+    dt4: "Efficiency",
+    dd4: "that makes the effort profitable in all ways.",
+    dt5: "Sustainability",
+    dd5: "that guarantees the present and the future.",
+    dt6: "Social Responsibility",
+    dd6: "that encompasses everything.",
+  },
+  aboutPresident: {
+    h1: "President’s Message",
+    p1: "“As the founder of 828 LLC, it is my responsibility to always maintain the integrity environment of the company, combined with the highest standards of quality, efficiency, reliability, and customer service. The technical aspect, the knowledge of what we do, is obviously very important to be able to provide our services. But we believe that they should be combined with the best customer service possible. We are “freaks” of customer service, and are totally convinced that without it, both our company and the relationships with our clients would not be alive on a long-term basis. We are committed to providing the best possible technical knowledge and implementation of that knowledge in HVAC, in engineering, and in projects in general. That leads us to the complete trust of our clients. We are a team, and every aspect of our business is managed with that in mind. If you have tried us, I hope that you had the best experience. And if not, please give us the opportunity to do what we do. Hard work, dedication!”",
+    p2: "Eng. Geraldo Peña Guzmán, PE, MEM",
+  },
+};
+
+const ES = {};
