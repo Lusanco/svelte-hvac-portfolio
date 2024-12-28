cde44b1ec16414bedb383cd73204331c87af321b
Lusanco <93743011+Lusanco@users.noreply.github.com>
2024-12-27 23:36:24 -0400

    update - base path

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/svelte-hvac-portfolio/", // Only for gh-pages, delete otherwise
   plugins: [svelte()],
   build: {
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
         chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  }
-})
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+});
